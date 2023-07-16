e7982d3331f68c13a160cba65d81727c251ff035
Geunyoung Kim <dea314@naver.com>
2023-07-16 23:33:48 +0900

    HTML 연결하기

--- web/board/views.py
+++ web/board/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
 # Create your views here.
+def index(req):
+    posts = models.post.objects.all()
+    return render(req, 'templates/board/index.html', {
+        'post_list' : posts
+    })
